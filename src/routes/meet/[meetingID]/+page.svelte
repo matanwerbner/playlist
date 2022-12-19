6083d2073fec585f5debbb231990e6eeb500483a
matan werbner <verbmat@gmail.com>
2022-12-19 18:27:33 +0200

    meetingTable

--- src/routes/meet/[meetingID]/+page.svelte
+++ src/routes/meet/[meetingID]/+page.svelte
@@ -1,28 +1,41 @@
 <script>
+	import { page } from '$app/stores';
 	import { Days } from '$lib/consts';
+	import MeetingTableCell from '../meetingTableCell.svelte';
+	import MeetingTable from '../meetingTable.svelte';
+	$: session = $page.data.session;
 
 	/** @type {import('./$types').PageData} */
 	export let data;
+	const isAttending = () => data.members[session.userID] === true;
+	const dayName = Days.find((d) => d.id == data.day)?.name;
 </script>
 
-<p>
-	<label for="meetingTitle"> כותרת: </label>
-	{data.title}
-</p>
-<p>
-	<label for="meetingDescription">מה עושים:</label>
-	{data.description}
-</p>
-
-<p>
-	<label for="meetingPlace"> איפה נפגשים: </label>
-	{data.location}
-</p>
-<p class="meetingDateContainer">
-	<label for="meetingDate">באיזה יום?</label>
-	{Days.find((d) => d.id == data.day)?.name}
-</p>
-<p class="meetingTimeContainer">
-	<label for="meetingTime">באיזו שעה?</label>
-	{data.time}
-</p>
+{#if isAttending()}
+	<button class="btn btn-blue">מגיע</button>
+{:else}
+	<button class="btn btn-blue">לא מגיע</button>
+{/if}
+<MeetingTable>
+	<tr>
+		<MeetingTableCell>כותרת:</MeetingTableCell>
+		<MeetingTableCell className="meetingTableValue">{data.title}</MeetingTableCell>
+	</tr>
+	<tr>
+		<MeetingTableCell>מה עושים:</MeetingTableCell>
+		<MeetingTableCell>{data.description}</MeetingTableCell>
+	</tr>
+	<tr>
+		<MeetingTableCell>איפה נפגשים:</MeetingTableCell>
+		<MeetingTableCell>{data.location}</MeetingTableCell>
+	</tr>
+	<tr>
+		<MeetingTableCell>באיזה יום?</MeetingTableCell>
+		<MeetingTableCell>{dayName}</MeetingTableCell>
+	</tr>
+	<tr>
+		<MeetingTableCell>באיזו שעה?</MeetingTableCell>
+		<MeetingTableCell>{data.time}</MeetingTableCell>
+	</tr>
+</MeetingTable>
+{session?.userID == data.adminID ? 'admin' : 'non-admin'}
---
+++ src/routes/meet/meetingTable.svelte
@@ -0,0 +1,7 @@
+<table
+	class="border-collapse border-hidden rounded-xl w-full border border-slate-400 bg-white text-lg shadow-lg"
+>
+	<tbody>
+		<slot />
+	</tbody>
+</table>
