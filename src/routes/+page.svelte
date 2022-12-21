3f6c899aaeac04a375744eaa1fbc31e80339c836
matan werbner <verbmat@gmail.com>
2022-12-21 12:15:27 +0200

    homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,24 @@
+<script>
+	import Going from '$lib/icons/going.svelte';
+	import collage from '$lib/images/collage.jpg';
+</script>
+
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
-
-<section />
+<div class="p-3 m-3">
+	<h1>ברוכים הבאים לplaylist !</h1>
+	<h2>האפליקציה שמארגנת לכם את המפגשים!</h2>
+	<h3>
+		רוצים לארגן מפגש עם החברים?<br />
+		צרו פגישה ושתפו אותה בwhatsapp
+		<br />
+		ותוכלו לראות מי מגיע ומי לא
+		<Going />
+	</h3>
+	<img class="mt-7" src={collage} alt="collage" />
+</div>
 
 <style lang="scss">
 </style>
